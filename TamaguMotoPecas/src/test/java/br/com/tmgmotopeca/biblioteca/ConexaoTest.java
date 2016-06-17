235d6c758d3817f63463ac3f3364ac6bdc69ca69
ResVUT42 <ResVUT42@192.168.107.215>
2016-06-17 20:24:51 -0300

    RESVut42 ElasticSearch

--- TamaguMotoPecas/src/test/java/br/com/tmgmotopeca/biblioteca/ConexaoTest.java
+++ TamaguMotoPecas/src/test/java/br/com/tmgmotopeca/biblioteca/ConexaoTest.java
@@ -1,38 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.com.tmgmotopeca.biblioteca;
-
-import java.sql.Connection;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-/**
- *
- * @author Thayro
- */
-public class ConexaoTest {
-
-    public ConexaoTest() {
-    }
-
-    /**
-     * Test of conectar method, of class Conexao.
-     */
-    @Test
-    public void testConectar() {
-        System.out.println("conectar");
-
-        Connection con = Conexao.conectar();
-
-        if (con == null) {
-            assertNotNull("Erro de conexão", con);
-        } else {
-            assertNotNull("Conexão realizada com sucesso!!!", con);
-        }
-
-    }
-
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package br.com.tmgmotopeca.biblioteca;
+//
+//import java.sql.Connection;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//
+///**
+// *
+// * @author Thayro
+// */
+//public class ConexaoTest {
+//
+//    public ConexaoTest() {
+//    }
+//
+//    /**
+//     * Test of conectar method, of class Conexao.
+//     */
+//    @Test
+//    public void testConectar() {
+//        System.out.println("conectar");
+//
+//        Connection con = Conexao.conectar();
+//
+//        if (con == null) {
+//            assertNotNull("Erro de conexão", con);
+//        } else {
+//            assertNotNull("Conexão realizada com sucesso!!!", con);
+//        }
+//
+//    }
+//
+//}
---
+++ TamaguMotoPecas/src/test/java/br/com/tmgmotopeca/biblioteca/TesteES.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tmgmotopeca.biblioteca;
+
+/**
+ *
+ * @author ResVUT42
+ */
+public class TesteES {
+    
+    
+    public TesteES() {
+        ConexaoES es  = ConexaoES.getInstance();
+    }
+    
+    public static void main(String[] args) {
+        new TesteES();
+    }
+}
